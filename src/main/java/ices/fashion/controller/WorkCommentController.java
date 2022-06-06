a3b4ba004977005b4774147fe47beaae0dee09ff
euphonium1998 <814811223@qq.com>
2022-06-06 23:12:15 +0800

    Update WorkCommentController.java

--- src/main/java/ices/fashion/controller/WorkCommentController.java
+++ src/main/java/ices/fashion/controller/WorkCommentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 @RequestMapping("/comment")
-public class CommentController {
+public class WorkCommentController {
 
     @Autowired
     private CommentService commentService;
